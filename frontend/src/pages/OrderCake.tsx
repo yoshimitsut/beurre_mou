ba39729be71d1afe98a1cf9286fd31e76c54268e
yoshimitsut <shimitsutanaka@gmail.com>
2025-11-06 21:10:27 -0300

    bug data

--- frontend/src/pages/OrderCake.tsx
+++ frontend/src/pages/OrderCake.tsx
@@ -45,9 +45,13 @@ export default function OrderCake() {
   const diasEspecificosPorMes = [
     { day: 2, month: 10 }, { day: 3, month: 10 }, { day: 9, month: 10 }, { day: 10, month: 10 },
     { day: 16, month: 10 }, { day: 17, month: 10 }, { day: 23, month: 10 }, { day: 24, month: 10 },
-    { day: 30, month: 11 }, { day: 1, month: 11 }, { day: 7, month: 11 }, { day: 8, month: 11 },
-    { day: 9, month: 11 }, { day: 13, month: 11 }, { day: 18, month: 11 }, { day: 19, month: 11 },
-    { day: 25, month: 12 }, { day: 26, month: 12 }, { day: 4, month: 12 }, { day: 5, month: 12 },
+    { day: 30, month: 11 }, 
+    
+    { day: 1, month: 11 }, { day: 7, month: 11 }, { day: 8, month: 11 }, { day: 14, month: 11 }, { day: 15, month: 11 }, 
+    { day: 22, month: 11 }, { day: 23, month: 11 },
+    { day: 24, month: 11 },{ day: 25, month: 11 }, { day: 26, month: 11 },
+
+    { day: 4, month: 12 }, { day: 5, month: 12 }, { day: 6, month: 12 },
     { day: 7, month: 12 }, { day: 8, month: 12 }, { day: 9, month: 12 }, { day: 13, month: 12 },
     { day: 18, month: 12 }, { day: 19, month: 12 }, { day: 25, month: 12 }, { day: 26, month: 12 },
   ];
