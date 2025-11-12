0271f86b3946a00501b85b77158c05f60fc4432f
yoshimitsut <shimitsutanaka@gmail.com>
2025-11-12 18:02:45 -0300

    add admin -> date

--- frontend/src/pages/TimeSlotsManagement.tsx
+++ frontend/src/pages/TimeSlotsManagement.tsx
@@ -38,7 +38,7 @@ const formatDate = (date: Date): string => {
   return [year, month, day].join('-');
 };
 
-const API_BASE_URL = 'http://localhost:3001/api/timeslots';
+const API_BASE_URL = import.meta.env.API_BASE_URL+'/api/timeslots';
 
 type TabType = 'times' | 'days';
 
