a161f965e984e526f450fbf2d0d42ee60d50c001
yoshimitsut <shimitsutanaka@gmail.com>
2025-11-14 12:19:25 -0300

    edit input

--- frontend/src/pages/OrderCake.tsx
+++ frontend/src/pages/OrderCake.tsx
@@ -616,7 +616,7 @@ const customStylesHour: StylesConfig<TimeOptionType, false> = {
         checked={item.fruit_option === "無し"}
         onChange={() => updateCake(index, "fruit_option", "無し")}
       />
-      <span>通常盛り</span><span style={{width:"50px", textAlign: "end"}}>+0円</span> 
+      <span style={{width:"90px", textAlign: "start"}}>通常盛り</span><span style={{width:"50px", textAlign: "end"}}>+0円</span> 
     </label>
     <label className={`pill ${item.fruit_option === "有り" ? "active" : ""}`}>
       <input
@@ -626,7 +626,7 @@ const customStylesHour: StylesConfig<TimeOptionType, false> = {
         checked={item.fruit_option === "有り"}
         onChange={() => updateCake(index, "fruit_option", "有り")}
       />
-      <span>フルーツ増し</span><span style={{width:"50px", textAlign: "end"}}>+648円</span>      
+      <span style={{width:"90px", textAlign: "start"}}>フルーツ増し</span><span style={{width:"50px", textAlign: "end"}}>+648円</span>      
     </label>
   </div>
   <label className='select-group-radio'>*フルーツ盛り</label>
