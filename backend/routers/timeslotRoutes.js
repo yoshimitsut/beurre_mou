1bbd2744bb3bc813951e61f99c76505f92a54765
yoshimitsut <shimitsutanaka@gmail.com>
2025-11-19 07:17:49 -0300

    alterando admin date

--- backend/routers/timeslotRoutes.js
+++ backend/routers/timeslotRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../config/db'); 
+const pool = require('../db'); 
 
 // --------------------------------------------------------------------------------
 // Rota de Inserção em Lote (POST /api/timeslots/batch)
